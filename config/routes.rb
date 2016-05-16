c4ac9c7fe795c3bc1b41d14220ce1b66912673c9
JefDev <jefmichielsen80dev@gmail.com>
2016-05-16 18:46:37 +0000

    Added validations and create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   
+ resources :contacts
+ #     contacts GET    /contacts(.:format)          contacts#index
+ #              POST   /contacts(.:format)          contacts#create
+ #  new_contact GET    /contacts/new(.:format)      contacts#new
+ # edit_contact GET    /contacts/:id/edit(.:format) contacts#edit
+ #      contact GET    /contacts/:id(.:format)      contacts#show
+ #              PATCH  /contacts/:id(.:format)      contacts#update
+ #              PUT    /contacts/:id(.:format)      contacts#update
+ #              DELETE /contacts/:id(.:format)      contacts#destroy
+ 
+ get '/about' => 'pages#about'
+ root 'pages#home'
+ 
 
-  root 'pages#home'
-  get '/about' => 'pages#about'
-
-  end
+end
